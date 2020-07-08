c2fc76725598c7cb4268781be624732833ca6095
Pixelastic <tim@pixelastic.com>
2020-07-08 17:14:08 +0200

    fix(records): remove deleted posts, update images and tags

--- scripts/data-records.js
+++ scripts/data-records.js
@@ -5,9 +5,12 @@ const pMap = require('golgoth/lib/pMap');
  * Download all the records for a given month and year
  **/
 (async () => {
+  process.setMaxListeners(Infinity);
   const subreddit = 'dndmaps';
   const months = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
   const years = [2020, 2019, 2018, 2017];
+  // await records.fromMonth(subreddit, 2020, 1);
+  // return;
   await pMap(
     years,
     async (year) => {
@@ -16,9 +19,9 @@ const pMap = require('golgoth/lib/pMap');
         async (month) => {
           await records.fromMonth(subreddit, year, month);
         },
-        { concurrency: 1 }
+        { concurrency: 5 }
       );
     },
-    { concurrency: 1 }
+    { concurrency: 5 }
   );
 })();
