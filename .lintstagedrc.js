5b1ced74c7c17525056f374afa84fe7ac48402c1
Pixelastic <tim@pixelastic.com>
2020-07-24 11:26:20 +0200

    fix(lint): Stop linting data JSON files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,10 @@
 const config = require('aberlaas/lib/configs/lintstaged.js');
+
+// Stop linting all the json files
+const jsonLintCommand = config['*.json'];
+delete config['*.json'];
+
 module.exports = {
   ...config,
+  './*.json': jsonLintCommand,
 };
