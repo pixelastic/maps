7a914234634544cc64c20f591e0f99fbd4c8c13b
Pixelastic <tim@pixelastic.com>
2020-07-24 11:22:10 +0200

    fix(records): Add score.isCurated and normalize tags

--- scripts/data-initial.js
+++ scripts/data-initial.js
@@ -1,6 +1,21 @@
 const reddinx = require('reddinx');
+const curatedAuthors = require('../lib/curatedAuthors.js');
+const _ = require('golgoth/lib/lodash');
 
 (async () => {
   const subredditName = 'dndmaps';
-  await reddinx.initial(subredditName);
+  await reddinx.initial(subredditName, {
+    onEach(record) {
+      // Remove "Maps" from tags
+      record.tags = _.map(record.tags, (tag) => {
+        return tag.replace(' Map', '');
+      });
+
+      // Mark as curated
+      const authorName = _.get(record, 'author.name');
+      record.score.isCurated = _.includes(curatedAuthors, authorName);
+
+      return record;
+    },
+  });
 })();
