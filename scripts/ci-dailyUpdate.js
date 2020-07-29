2c1ed4b653accff5fbfba90eae5dcf1cf6774cf2
Pixelastic <tim@pixelastic.com>
2020-07-29 22:23:51 +0200

    wip(ci): Test CI

--- scripts/ci-dailyUpdate.js
+++ scripts/ci-dailyUpdate.js
@@ -3,26 +3,26 @@ const dayjs = require('golgoth/lib/dayjs');
 const run = require('firost/lib/run');
 
 (async () => {
-  await onCircle.run(async ({ gitChangedFiles, gitCommitAll, gitPush }) => {
-    await run('yarn run data:incremental');
+  await onCircle.run(
+    async ({ success, gitChangedFiles, gitCommitAll, gitPush }) => {
+      await run('yarn run data:incremental');
 
-    if (!(await gitChangedFiles())) {
-      console.info('No files changed, we stop early');
-      return true;
+      if (!(await gitChangedFiles())) {
+        success('No files changed, we stop early');
+      }
+
+      // Commit changes
+      const currentDate = dayjs.utc().format('YYYY-MM-DD');
+      const commitMessage = `chore(ci): Daily update (${currentDate})`;
+      await gitCommitAll(commitMessage);
+
+      // And push
+      await gitPush();
+
+      // Re-index into Algolia
+      await run('yarn run data:indexing');
+
+      success('Daily data indexed');
     }
-
-    // Commit changes
-    const currentDate = dayjs.utc().format('YYYY-MM-DD');
-    const commitMessage = `chore(ci): Daily update (${currentDate})`;
-    await gitCommitAll(commitMessage);
-
-    // And push
-    await gitPush();
-
-    // Re-index into Algolia
-    await run('yarn run data:indexing');
-
-    console.info('All steps run correctly');
-    return true;
-  });
+  );
 })();
