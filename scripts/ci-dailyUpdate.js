030dec57540361e1baf328a54ddd08e0cf928950
Pixelastic <tim@pixelastic.com>
2020-07-30 00:03:40 +0200

    fix(dailyUpdate): Add some logging information

--- scripts/ci-dailyUpdate.js
+++ scripts/ci-dailyUpdate.js
@@ -1,13 +1,16 @@
 const onCircle = require('on-circle');
 const dayjs = require('golgoth/lib/dayjs');
 const run = require('firost/lib/run');
+const consoleInfo = require('firost/lib/consoleInfo');
+const _ = require('golgoth/lib/lodash');
 
 (async () => {
   await onCircle.run(
     async ({ success, gitChangedFiles, gitCommitAll, gitPush }) => {
       await run('yarn run data:incremental');
 
-      if (!(await gitChangedFiles())) {
+      const changedFiles = await gitChangedFiles();
+      if (_.isEmpty(changedFiles)) {
         success('No files changed, we stop early');
       }
 
@@ -15,14 +18,16 @@ const run = require('firost/lib/run');
       const currentDate = dayjs.utc().format('YYYY-MM-DD');
       const commitMessage = `chore(ci): Daily update (${currentDate})`;
       await gitCommitAll(commitMessage);
+      consoleInfo(`${changedFiles.length} files changed`);
 
       // And push
       await gitPush();
+      consoleInfo('Changes pushed to the repo');
 
       // Re-index into Algolia
       await run('yarn run data:indexing');
 
-      success('Daily data indexed');
+      success('Daily data updated');
     }
   );
 })();
