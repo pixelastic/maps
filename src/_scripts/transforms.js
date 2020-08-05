d1aef4ad374b0656d02f45192d713425331c06b7
Pixelastic <tim@pixelastic.com>
2020-08-05 15:54:40 +0200

    fix(images): Use images.weserv.nl instead of Cloudinary

--- src/_scripts/transforms.js
+++ src/_scripts/transforms.js
@@ -1,17 +1,9 @@
-const cloudinary = require('norska/frontend/cloudinary');
-const proxy = require('norska/frontend/cloudinary/proxy');
-cloudinary.init(window.CONFIG.cloudinary);
 const lazyloadAttributes = require('norska/frontend/lazyload/attributes');
 
 module.exports = {
   preview(item) {
-    const previewUrl =
-      item.picture.preview || 'https://placekitten.com/408/287';
+    const previewUrl = item.picture.preview;
     const options = { width: 600, placeholder: { width: 200 } };
     return lazyloadAttributes(previewUrl, options);
-  },
-  zoomedPicture(item) {
-    const options = { width: 1024 };
-    return proxy(item.picture.full, options);
   },
 };
