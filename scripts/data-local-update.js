68a223f25078e3fd04a9129b1c1e3c7d2a30e194
Tim Carry <tim@pixelastic.com>
2021-03-17 17:01:12 +0100

    Update local data

--- scripts/data-local-update.js
+++ scripts/data-local-update.js
@@ -3,15 +3,20 @@ const writeJson = require('firost/writeJson');
 const glob = require('firost/glob');
 const pMap = require('golgoth/pMap');
 const onEach = require('../lib/onEach.js');
+const spinner = require('firost/spinner');
 
 // Update all local .json files by applying onEach again on them.
 // This is useful when, for example, updating the list of all curated authors
 // and having it retro-actively mark previous posts as curated
 (async () => {
   const files = await glob('./data/**/*.json');
+  const max = files.length;
+  const progress = spinner.start(max);
   await pMap(files, async (filepath) => {
     const currentData = await readJson(filepath);
     const newData = onEach(currentData);
     await writeJson(newData, filepath);
+    progress.tick();
   });
+  progress.success('All files updated');
 })();
