2fb85106894063d49e2265ac517317355580e2d5
Tim Carry <tim@pixelastic.com>
2021-07-06 00:35:04 +0200

    Change the indexName

--- src/_data/config.js
+++ src/_data/config.js
@@ -2,6 +2,6 @@ module.exports = {
   algolia: {
     appId: 'O3F8QXYK6R',
     apiKey: 'fc6f75fa8bec521693bb35881a46af18',
-    indexName: 'gamemaster_maps_dev',
+    indexName: 'gamemaster_maps',
   },
 };
